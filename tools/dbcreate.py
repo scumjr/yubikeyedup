ecfb9cd74a68ca21e4d39fa58f8ec8025f338035
scumjr <scumjr@users.noreply.github.com>
2015-08-17 22:32:32 +0200

    dbcreate: use sqlite3 python module to create database

--- tools/dbcreate.py
+++ tools/dbcreate.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
 import os
-import subprocess
+import sqlite3
 import sys
 
-SCHEMA = '''
-BEGIN TRANSACTION;
-create table yubikeys(
+TABLES = [
+'''yubikeys(
  nickname varchar(16) unique not null,
  publicname varchar(16) unique not null,
  created varchar(24) not null,
@@ -15,36 +14,33 @@ create table yubikeys(
  active boolean default true,
  counter integer not null default 1,
  time integer not null default 1
-);
-create table oathtokens(
+)''',
+'''oathtokens(
  nickname varchar(16) unique not null,
  publicname varchar(12) unique not null,
  created varchar(24) not null,
  secret varchar(40) not null,
  active boolean default true,
  counter integer not null default 1
-);
-create table apikeys(
+)''',
+'''apikeys(
  nickname varchar(16),
  secret varchar(28),
  id integer primary key
-);
-COMMIT;
-'''
+)''',
+]
 
 def create_db(filename):
     if os.path.exists(filename):
         print '%s already exists' % filename
         sys.exit(1)
 
-    p = subprocess.Popen([ 'sqlite3', filename ], stdin=subprocess.PIPE)
-    p.stdin.write(SCHEMA)
-    p.stdin.close()
-
-    if p.wait() != 0:
-        print 'failed to create %s' % filename
-        sys.exit(1)
-
+    conn = sqlite3.connect(filename)
+    c = conn.cursor()
+    for table in TABLES:
+        c.execute('CREATE TABLE %s' % table)
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
